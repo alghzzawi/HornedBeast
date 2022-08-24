3e286ee661a702e823a496fd78c97c5a95dbe476
alghzzawi <mohammadalghazzawi9@gmail.com>
2022-08-24 23:45:37 +0300

    add filter horn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 class Footer extends React.Component{
     render(){
         return(
-            <div>
+            <div id="foorter">
                 <p>&copy; copyRights Mohammad Alghzawi 2022</p>
             </div>
         )
